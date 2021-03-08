cbebf9918e2f2b4b49d26ec497eb69fc65c9c4d5
Jonas Almeida <jalmeida@mathbiol.org>
2021-03-08 17:40:47 -0500

    pgs.pako

--- pgs.js
+++ pgs.js
@@ -2,8 +2,28 @@ console.log('pgs.js loaded')
 
 pgs = {date:Date()}
 
+pgs.loadScript=async(url)=>{
+    let s = document.createElement('script')
+    s.src=url
+    return document.head.appendChild(s)
+}
 
+
+pgs.loadPako=function(){
+    pgs.loadScript("https://cdnjs.cloudflare.com/ajax/libs/pako/2.0.3/pako.min.js").then(s=>{
+        s.onload=function(){
+            pgs.pako=pako
+        }
+    })      
+}
 
 if(typeof(define)!="undefined"){
-    define (pgs)
+    //define(pgs)
+    define(['https://cdnjs.cloudflare.com/ajax/libs/pako/2.0.3/pako.min.js'],function(pako){
+        pgs.pako = pako
+        return pgs
+    })
+}else{
+    pgs.loadPako()
 }
+
