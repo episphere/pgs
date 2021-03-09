34f1124ee9217b0a0cee5cae9feec1414fbebfa8
Jonas Almeida <jalmeida@mathbiol.org>
2021-03-08 19:49:13 -0500

    textArea

--- pgs.js
+++ pgs.js
@@ -14,6 +14,17 @@ pgs.loadScore=async(entry='PGS000004')=>{
     return pgs.pako.inflate(await (await fetch(url)).arrayBuffer(),{to:'string'})
 }
 
+pgs.textArea = async (entry='PGS000004')=>{
+    let txt = await pgs.loadScore(entry);
+    let ta = document.createElement('textarea'); //DOM.element('textarea');
+    ta.value = txt;
+    ta.style.width = '100%';
+    ta.style.height = '20em';
+    ta.style.color = 'lime';
+    ta.style.backgroundColor = 'black';
+  return ta;
+}
+
 
 pgs.loadPako=function(){
     pgs.loadScript("https://cdnjs.cloudflare.com/ajax/libs/pako/2.0.3/pako.min.js").then(s=>{
